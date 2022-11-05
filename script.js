ac69f14d1d74a9fe37bb4d5d8ae1e1c4f683f9f5
Juan Sebastian Peña Angarita <101535377+codingSiteOne@users.noreply.github.com>
2022-11-04 23:27:59 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -21,4 +21,49 @@ var typed = new Typed('.typed', {
 	contentType: 'html',  // 'html' o 'null' para texto sin formato
 });
 
+const cuerpo = document.body;
+//console.log("probando mi js");
+const cambiarFondo = (body) => {
+		const seccion = document.getElementById('contenido');
+		let oscuro = "FONDO OSCURO";
+		let claro = "FONDO CLARO";
+		let contenidoBoton = document.getElementById('btnColor');
+
+		if(body.className == "default"){
+			//console.log(true);
+			body.style.backgroundImage = "url(images/rocket.jpg)";
+			body.style.backgroundRepeat = "no-repeat";
+			body.style.backgroundPosition = "center";
+			body.style.backgroundSize = "cover";
+			//cambiando el nombre de la clase
+			body.className = "other";
+			contenidoBoton.innerHTML = oscuro;
+			body.style.color = "#000";
+			fondoSeccion(seccion);
+			document.getElementById('myselfImg').style.width = "65%";
+			document.getElementById('myselfImg').style.boxShadow = "8px 5px 15px #000";
+			document.getElementById('myselfImg').style.height = "90%";
+		}
+		else{
+			//console.log(false);
+			body.style.background = "linear-gradient(135deg, #24242455 25%, transparent 25%) -10px 0/ 20px 20px, linear-gradient(225deg, #242424 25%, transparent 25%) -10px 0/ 20px 20px, linear-gradient(315deg, #24242455 25%, transparent 25%) 0px 0/ 20px 20px, linear-gradient(45deg, #242424 25%, #000000 25%) 0px 0/ 20px 20px";
+			//cambiando el nombre de la clase
+			body.className = "default";
+			contenidoBoton.innerHTML = claro;
+			body.style.color = "aliceblue";
+			seccion.style.background = "transparent";
+		}
+}
+
+const fondoSeccion = (elemento) => {
+	//console.log(elemento);
+	elemento.style.backgroundColor = "rgb(192, 181, 181)";
+	elemento.style.opacity = "0.9";
+	elemento.style.borderRadius = "40px";
+	elemento.style.padding = "25px";
+}
+
+
+
+
 
