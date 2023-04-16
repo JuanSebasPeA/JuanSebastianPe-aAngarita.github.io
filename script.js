ebd8defbaa6a2c7592cb4c353f90ee7c554d435b
Juan Sebastian Peña Angarita <101535377+JuanSebasPeA@users.noreply.github.com>
2023-04-15 22:15:47 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,69 +1,43 @@
-/* Author: Juan Sebastian Peña Angarita */
+//Función que me aplica el estilo a la opciòn seleccionada y quita la previamente seleccionada
+function seleccionar(link) {
+    var opciones = document.querySelectorAll('#links  a');
+    opciones[0].className = "";
+    opciones[1].className = "";
+    opciones[2].className = "";
+    opciones[3].className = "";
+    opciones[4].className = "";
+    link.className = "seleccionado";
 
-/* using the library "typed.js" to give a write machine effect */
-var typed = new Typed('.typed', {
-	strings: [
-		'<i class="info">Juan Sebastian Peña Angarita</i>',
-		'<i class="info">Programmer in process</i>',
-	],
-
-	// stringsElement: '#cadenas-texto', // ID del elemento que contiene cadenas de texto a mostrar.
-	typeSpeed: 75,  // Velocidad en mlisegundos para poner una letra,
-	startDelay: 300,  // Tiempo de retraso en iniciar la animacion. Aplica tambien cuando termina y vuelve a iniciar,
-	backSpeed: 75,  // Velocidad en milisegundos para borrrar una letra,
-	smartBackspace: true, // Eliminar solamente las palabras que sean nuevas en una cadena de texto.
-	shuffle: false, // Alterar el orden en el que escribe las palabras.
-	backDelay: 1500,  // Tiempo de espera despues de que termina de escribir una palabra.
-	loop: true,   // Repetir el array de strings
-	loopCount: false,   // Cantidad de veces a repetir el array.  false = infinite
-	showCursor: true,   // Mostrar cursor palpitanto
-	cursorChar: '|',   // Caracter para el cursor
-	contentType: 'html',  // 'html' o 'null' para texto sin formato
-});
-
-const cuerpo = document.body;
-//console.log("probando mi js");
-const cambiarFondo = (body) => {
-		const seccion = document.getElementById('contenido');
-		let oscuro = "FONDO OSCURO";
-		let claro = "FONDO CLARO";
-		let contenidoBoton = document.getElementById('btnColor');
-
-		if(body.className == "default"){
-			//console.log(true);
-			body.style.backgroundImage = "url(images/rocket.jpg)";
-			body.style.backgroundRepeat = "no-repeat";
-			body.style.backgroundPosition = "center";
-			body.style.backgroundSize = "cover";
-			//cambiando el nombre de la clase
-			body.className = "other";
-			contenidoBoton.innerHTML = oscuro;
-			body.style.color = "#000";
-			fondoSeccion(seccion);
-			document.getElementById('myselfImg').style.width = "65%";
-			document.getElementById('myselfImg').style.boxShadow = "8px 5px 15px #000";
-			document.getElementById('myselfImg').style.height = "90%";
-		}
-		else{
-			//console.log(false);
-			body.style.background = "linear-gradient(135deg, #24242455 25%, transparent 25%) -10px 0/ 20px 20px, linear-gradient(225deg, #242424 25%, transparent 25%) -10px 0/ 20px 20px, linear-gradient(315deg, #24242455 25%, transparent 25%) 0px 0/ 20px 20px, linear-gradient(45deg, #242424 25%, #000000 25%) 0px 0/ 20px 20px";
-			//cambiando el nombre de la clase
-			body.className = "default";
-			contenidoBoton.innerHTML = claro;
-			body.style.color = "aliceblue";
-			seccion.style.background = "transparent";
-		}
+    //Hacemos desaparecer el menu una vez que se ha seleccionado una opcion
+    //en modo responsive
+    var x = document.getElementById("nav");
+    x.className = "";
 }
 
-const fondoSeccion = (elemento) => {
-	//console.log(elemento);
-	elemento.style.backgroundColor = "rgb(192, 181, 181)";
-	elemento.style.opacity = "0.9";
-	elemento.style.borderRadius = "40px";
-	elemento.style.padding = "25px";
+//función que muestra el menu responsive
+function responsiveMenu() {
+    var x = document.getElementById("nav");
+    if (x.className === "") {
+        x.className = "responsive";
+    } else {
+        x.className = "";
+    }
 }
 
+//detecto el scrolling para aplicar la animación del la barra de habilidades
+window.onscroll = function() { efectoHabilidades() };
 
+//funcion que aplica la animación de la barra de habilidades
+function efectoHabilidades() {
+    var skills = document.getElementById("skills");
+    var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
+    if (distancia_skills >= 300) {
+        document.getElementById("html").classList.add("barra-progreso1");
+        document.getElementById("js").classList.add("barra-progreso1");
+        document.getElementById("rct").classList.add("barra-progreso2");
+        document.getElementById("nd").classList.add("barra-progreso4");
+        document.getElementById("java").classList.add("barra-progreso2");
+        document.getElementById("py").classList.add("barra-progreso4");
+    }
 
-
-
+}
