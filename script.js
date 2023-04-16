69c848eacd346b75081877fd228e330b7c32cea0
Juan Sebastian Peña Angarita <101535377+JuanSebasPeA@users.noreply.github.com>
2023-04-15 22:18:46 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//Función que me aplica el estilo a la opciòn seleccionada y quita la previamente seleccionada
+
 function seleccionar(link) {
     var opciones = document.querySelectorAll('#links  a');
     opciones[0].className = "";
@@ -8,13 +8,12 @@ function seleccionar(link) {
     opciones[4].className = "";
     link.className = "seleccionado";
 
-    //Hacemos desaparecer el menu una vez que se ha seleccionado una opcion
-    //en modo responsive
+    
     var x = document.getElementById("nav");
     x.className = "";
 }
 
-//función que muestra el menu responsive
+
 function responsiveMenu() {
     var x = document.getElementById("nav");
     if (x.className === "") {
@@ -24,10 +23,10 @@ function responsiveMenu() {
     }
 }
 
-//detecto el scrolling para aplicar la animación del la barra de habilidades
+
 window.onscroll = function() { efectoHabilidades() };
 
-//funcion que aplica la animación de la barra de habilidades
+
 function efectoHabilidades() {
     var skills = document.getElementById("skills");
     var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
